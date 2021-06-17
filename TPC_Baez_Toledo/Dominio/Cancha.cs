c40d2d62cf5642c73a65a8ba4bc9de379701eee4
LucasBaez-arg <71524921+LucasBaez-arg@users.noreply.github.com>
2021-06-16 23:48:18 -0300

    clases

---
+++ TPC_Baez_Toledo/Dominio/Cancha.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Cancha
+    {
+        //propiedades
+        private int id;
+        private decimal precio;
+        private TipoCancha tipoCancha;
+        private string descripcion;
+        private string urlImagen;
+        //metodos
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+        public decimal Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        public TipoCancha TipoCancha
+        {
+            get { return tipoCancha; }
+            set { tipoCancha = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        public string UrlImagen
+        {
+            get { return urlImagen; }
+            set { urlImagen = value; }
+        }
+
+    }
+}
+
---
+++ TPC_Baez_Toledo/Dominio/TipoCancha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class TipoCancha
+    {
+        ///Propediades
+        private int id;
+        private string nombre;
+        ///Metodos
+        
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        } 
+    }
+}
