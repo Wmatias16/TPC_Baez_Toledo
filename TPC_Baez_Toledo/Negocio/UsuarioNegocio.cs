87425c8444485394cf3e977041c3795e0c1adf93
Wmatias16 <wmatias1009@gmail.com>
2021-06-17 23:54:12 -0300

    Negocio, Acceso a datos

---
+++ TPC_Baez_Toledo/Negocio/UsuarioNegocio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dominio;
+
+namespace Negocio
+{
+    public class UsuarioNegocio
+    {
+        public void Agregar(Usuario usuario)
+        {
+            AccesoDatos db = new AccesoDatos();
+
+            try
+            {
+                string value = "values('" + usuario.Rol + "', '" + usuario.Nombre + "', '" + usuario.Apellidos + "', '" + usuario.Email + "', '" + usuario.Telefono + "')";
+                db.SetearConsulta("INSERT INTO Usuarios(Rol,Nombre,Apellido,Email,Telefono)"+ value);
+
+                db.EjectutarAccion();
+
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            finally
+            {
+                db.CerraConexion();
+            }
+        }
+    }
+}
